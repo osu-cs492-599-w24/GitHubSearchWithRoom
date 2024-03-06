4b8b9e94ccff1f14efed0af90110ec594a183e66
Rob Hess <hessro@oregonstate.edu>
2024-03-05 17:07:27 -0800

    Add code from 3/5 lecture.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.22-1.0.17")
 
     implementation("com.google.android.material:material:1.11.0")
 
@@ -58,6 +62,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+    ksp("androidx.room:room-compiler:2.6.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
